a597f3d1a2f406e5b8444194258cd8ded55c585f
JVillamizar <villamizardiaz.jorgehernando@gmail.com>
2026-02-18 10:29:19 -0500

    Update scottowing.h

--- miryoku/mapping/34/scottowing.h
+++ miryoku/mapping/34/scottowing.h
@@ -22,6 +22,13 @@ XXX K20 K21 K22 K23 K24  K25 K26 K27 K28 K29 XXX \
    (K32 and K37 will be &none under 34-key thumb tables.) */ \
 K33 K34 K35 K36
 
+/* Ferris-style thumb kludge, but with ScottoWing thumb indices:
+   3 rows × 12 = 36, then thumbs are 36..39 */
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 36 37
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 38 39
+#define MIRYOKU_KLUDGE_THUMBCOMBOS
+
 #endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_SCOTTOWING
+
